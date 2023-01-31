fe5068edebd37b18917730673718c25cb5fcda1d
Stephan Brandt <stephan.brandt@contagt.com>
2023-01-31 11:28:30 +0100

    Fix: Build for Super Dev Mode

---
+++ modules/gwt/src/main/java/org/locationtech/jtsemul/java/text/DecimalFormat.java
@@ -0,0 +1,15 @@
+package java.text;
+
+public class DecimalFormat {
+
+    public DecimalFormat(String var1, DecimalFormatSymbols var2) {
+        /*this.roundingMode = RoundingMode.HALF_EVEN;
+        this.isFastPath = false;
+        this.fastPathCheckNeeded = true;
+        this.serialVersionOnStream = 4;
+        this.symbols = (DecimalFormatSymbols)var2.clone();
+        this.applyPattern(var1, false);*/
+    }
+
+    public String format(double val) { return null; }
+}
---
+++ modules/gwt/src/main/java/org/locationtech/jtsemul/java/text/DecimalFormatSymbols.java
@@ -0,0 +1,7 @@
+package java.text;
+
+public class DecimalFormatSymbols {
+    public DecimalFormatSymbols() {}
+
+    public void setDecimalSeparator(char var1) {}
+}
