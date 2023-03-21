09449cd24a2aa157c58a55d949ec1cf84baf59c6
Stephan Brandt <stephan.brandt@contagt.com>
2023-03-21 14:49:04 +0100

    Add: Edge of MonotoneChainEdge -> Weak

--- modules/core/src/main/java/org/locationtech/jts/geomgraph/index/MonotoneChainEdge.java
+++ modules/core/src/main/java/org/locationtech/jts/geomgraph/index/MonotoneChainEdge.java
@@ -11,6 +11,8 @@
  */
 package org.locationtech.jts.geomgraph.index;
 
+import com.google.j2objc.annotations.Weak;
+
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Envelope;
 import org.locationtech.jts.geomgraph.Edge;
@@ -35,6 +37,7 @@ import org.locationtech.jts.geomgraph.Edge;
  */
 public class MonotoneChainEdge {
 
+  @Weak
   Edge e;
   Coordinate[] pts; // cache a reference to the coord array, for efficiency
   // the lists of start/end indexes of the monotone chains.
